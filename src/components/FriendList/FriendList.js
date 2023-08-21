c9d679a19ee43d01e8981d3f19e118b17ecfaf05
Vika14 <manunya1406@gmail.com>
2023-08-21 13:15:08 +0200

    hw1-4

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,24 +1,17 @@
 import PropTypes from 'prop-types';
 import css from './friendList.module.css';
+import { FriendListItem } from './FriendListItem/FriendListItem';
 
 export const FriendList = ({ friends }) => {
   return (
     <ul className={css.friendlist}>
-      {friends.map(friend => (
-        <li className={css.item}>
-          <span
-            className={
-              friend.isOnline ? `${css.status} ${css.isOnline}` : css.status
-            }
-          ></span>
-          <img
-            className={css.avatar}
-            src={friend.avatar}
-            alt="User avatar"
-            width="48"
-          />
-          <p className={css.name}>{friend.name}</p>
-        </li>
+      {friends.map(({ id, avatar, isOnline, name }) => (
+        <FriendListItem
+          key={id}
+          name={name}
+          avatar={avatar}
+          isOnline={isOnline}
+        />
       ))}
     </ul>
   );
